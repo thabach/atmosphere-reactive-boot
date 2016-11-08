d86a6939a4fee577bf7cc2b8057a3073ec2e9b1e
jfarcand <jfarcand@apache.org>
2016-11-08 15:26:03 -0500

    Add TypingService

---
+++ minsk/src/main/java/com/yulplay/reactive/boot/service/ClientTypingService.java
@@ -0,0 +1,40 @@
+/**
+ * Copyright 2015 Yulplay.com
+ */
+package com.yulplay.reactive.boot.service;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.yulplay.protocol.Enveloppe;
+import com.yulplay.reactive.boot.ReactiveWebSocketFactory;
+import com.yulplay.reactive.boot.Reply;
+import com.yulplay.reactive.boot.Service;
+import com.yulplay.reactive.boot.annotation.On;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.inject.Inject;
+import java.io.IOException;
+
+@On("/typing")
+public class ClientTypingService implements Service<Enveloppe> {
+    private final Logger logger = LoggerFactory.getLogger(MessageDispatcherService.class);
+
+    @Inject
+    private ReactiveWebSocketFactory webSocketFactory;
+
+    @Inject
+    private ObjectMapper mapper;
+
+    @Override
+    public void on(Enveloppe enveloppe, Reply<Enveloppe> reply) throws IOException {
+        byte[] b64Enveloppe = mapper.writeValueAsBytes(enveloppe);
+        webSocketFactory.findAll().parallelStream()
+                .forEach(w -> {
+                    try {
+                        w.write(b64Enveloppe, 0, b64Enveloppe.length);
+                    } catch (IOException e) {
+                        logger.warn("", e);
+                    }
+                });
+    }
+}
